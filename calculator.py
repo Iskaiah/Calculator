e16a67aaf2bf7557042d5b3a5c0f1af53a73cb0c
Isaiah Katoboe <katoboeisaiah@gmail.com>
2026-05-22 10:41:12 +1100

    create calculator function

--- calculator.py
+++ calculator.py
@@ -4,4 +4,15 @@ def multiply(x, y): return x * y
 def divide(x, y): 
     if y != 0:
         return x / y
-    
+
+def calculator():
+    print("Select Operation: +, -, / or 'q' to quit")
+    while True:
+        choice = input("Operation: ")
+        if choice == 'q': break
+        try:
+            n1 = float(input("Num 1: "))
+        
+        except ValueError: Print("Invalid number")
+
+if __name__ == "_main_": calculator()
