638b1a9778cb8dcf5c7953d49bf14f216fb2d60e
Isaiah Katoboe <katoboeisaiah@gmail.com>
2026-05-22 11:26:00 +1100

    add few changes and structure

--- calculator.py
+++ calculator.py
@@ -1,23 +1,33 @@
-def add (x, y): return x + y
-def subtract(x, y): return x-y
-def multiply(x, y): return x * y
-def divide(x, y): 
-    if y != 0:
-        return x / y
+def add(n1, n2):
+    return n1 + n2
 
-def calculator():
-    print("Select Operation: +, -, / or 'q' to quit")
-    while True:
-        choice = input("Operation: ")
-        if choice == 'q': break
-        try:
-            n1 = float(input("Num 1: "))
-            n2 = float(input("Num 2: "))
-            if choice == '+': print(add(n1, n2))
-            elif choice == '-': print(subtract(n1, n2))
-            elif choice == '*': print(multiply(n1, n2))
-            elif choice == "/": print(divide(n1, n2))
-        
-        except ValueError: print("Invalid number")
+def sub(n1, n2):
+    return n1 - n2
 
-if __name__ == "_main_": calculator()
+def mul(n1, n2):
+    return n1 * n2
+
+def div(n1, n2):
+    return n1 / n2
+
+print("Please select operation -\n"
+      "1. Add\n"
+      "2. Subtract\n"
+      "3. Multiply\n"
+      "4. Divide\n")
+
+sel = int(input("Select operation (1-4): "))
+
+n1 = int(input("Enter first number: "))
+n2 = int(input("Enter second number: "))
+
+if sel == 1:
+    print(n1, "+", n2, "=", add(n1, n2))
+elif sel == 2:
+    print(n1, "-", n2, "=", sub(n1, n2))
+elif sel == 3:
+    print(n1, "*", n2, "=", mul(n1, n2))
+elif sel == 4:
+    print(n1, "/", n2, "=", div(n1, n2))
+else:
+    print("Invalid input")
