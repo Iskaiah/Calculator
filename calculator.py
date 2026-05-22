c88dd946e1622eecee8c23fa2c62e5018408efc7
Isaiah Katoboe <katoboeisaiah@gmail.com>
2026-05-22 11:02:02 +1100

    create choices

--- calculator.py
+++ calculator.py
@@ -12,7 +12,12 @@ def calculator():
         if choice == 'q': break
         try:
             n1 = float(input("Num 1: "))
+            n2 = float(input("Num 2: "))
+            if choice == '+': print(add(n1, n2))
+            elif choice == '-': print(subtract(n1, n2))
+            elif choice == '*': print(multiply(n1, n2))
+            elif choice == "/": print(divide(n1, n2))
         
-        except ValueError: Print("Invalid number")
+        except ValueError: print("Invalid number")
 
 if __name__ == "_main_": calculator()
